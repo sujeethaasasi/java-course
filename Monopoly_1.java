d4231f56268c17292edbd64fa905715af5f29671
msivass98 <msivass98@gmail.com>
2019-10-20 18:34:22 +0530

    Added code for storing playerlist array

--- Monopoly_1.java
+++ Monopoly_1.java
@@ -10,8 +10,11 @@ class Monopoly_1{
 		String tableType[]	= {"Add","Buy","PARK","TRAIN","JAIL","ELECTRICAL COMPANY","Bangalore","Coffee shop","Community chest","Play ground","Free car parking","Hyderabad","Water board","Go to Start","Goa","Mysore","income tax","Madurai","Beach house","Coimbatore","Go to Jail","Luxury tax","Market","Community chest","Auto stand","Estate","Udupi","Hotel","Coorg","Hubli","Bus station","America","London","Hospital","England","Paris","Bowling court","Europe","Kodaikanal","Clubs"};
 		String tableBuy[]	= {"2000","30000","PARK","TRAIN","JAIL","ELECTRICAL COMPANY","Bangalore","Coffee shop","Community chest","Play ground","Free car parking","Hyderabad","Water board","Go to Start","Goa","Mysore","income tax","Madurai","Beach house","Coimbatore","Go to Jail","Luxury tax","Market","Community chest","Auto stand","Estate","Udupi","Hotel","Coorg","Hubli","Bus station","America","London","Hospital","England","Paris","Bowling court","Europe","Kodaikanal","Clubs"};
 		String tableRent[]	= {"2000","1500","PARK","TRAIN","JAIL","ELECTRICAL COMPANY","Bangalore","Coffee shop","Community chest","Play ground","Free car parking","Hyderabad","Water board","Go to Start","Goa","Mysore","income tax","Madurai","Beach house","Coimbatore","Go to Jail","Luxury tax","Market","Community chest","Auto stand","Estate","Udupi","Hotel","Coorg","Hubli","Bus station","America","London","Hospital","England","Paris","Bowling court","Europe","Kodaikanal","Clubs"};
-		String playerlist1[]=new String[40];
-		String playerlist2[]=new String[40];
+		String playerList1[]=new String[40];
+		String playerList2[]=new String[40];
+
+		// int playerList[][]=new int[2][5];
+
 
 		// Table Type - Details
 		// Buy - 
@@ -19,7 +22,7 @@ class Monopoly_1{
 		// Add -
 		// Sub -
 
-		int i=1,k=0,l=0;
+		int i=1;
 
 		String playerName[]=new String[2];
 
@@ -48,9 +51,9 @@ class Monopoly_1{
 			System.out.println();
 		}
 
-		playerName[0] = startgame();
+		playerName[0] = getPlayerName();
 		//System.out.println(playerName[0]);
-		playerName[1] = startgame();
+		playerName[1] = getPlayerName();
 		// System.out.println(playerName[1]);
 		
 		int currentTurn = 0;
@@ -60,13 +63,14 @@ class Monopoly_1{
 		for(i=1;;)
 		{
 			
-			System.out.println(currentTurn);
+			System.out.println("Player Turn : " + currentTurn);
 
-			dice1 = p1dice();
-			dice2 = p1dice();
+			dice1 = diceRoll();
+			dice2 = diceRoll();
 			roll= dice1+dice2;
 
-			currentPointer[currentTurn] = currentPointer[currentTurn] + roll;
+			// currentPointer[currentTurn] = currentPointer[currentTurn] + roll;
+			int rollLocation = currentPointer[currentTurn] + roll;
 
 			// Example
 			// Roll = 5
@@ -84,28 +88,35 @@ class Monopoly_1{
 			System.out.println(dice2);
 			System.out.println(p1dice);
 */
-			if (currentPointer[currentTurn]>39) //if condition to start from 0 if dice value crosses 40
+			System.out.println("Dice Number is : " + roll);
+			System.out.println("Player Current Pointer : " + currentPointer[currentTurn]);
+			
+			if (rollLocation > 39) //if condition to start from 0 if dice value crosses 40
 			{
-				currentPointer[currentTurn]=currentPointer[currentTurn]-40;
-				System.out.println(currentPointer[currentTurn]);
-				System.out.println(tableName[currentPointer[currentTurn]]);
+				currentPointer[currentTurn] = rollLocation-40;
+				System.out.println("Current Roll Location : " + currentPointer[currentTurn]);
+				System.out.println("Current Table Name    : " + tableName[currentPointer[currentTurn]]);
 			}
 			else
 			{
-				System.out.println(currentPointer[currentTurn]);
-				System.out.println(tableName[currentPointer[currentTurn]]);
+				currentPointer[currentTurn] = rollLocation;
+				System.out.println("Current Roll Location : " + currentPointer[currentTurn]);
+				System.out.println("Current Table Name    : " + tableName[currentPointer[currentTurn]]);
 			}
 			
-				if (currentTurn==0)
-				{
-					playerlist1[k]=tableName[currentPointer[currentTurn]];
-					k++;
+			System.out.println("-------------" + "Play " + currentTurn + "-------------");
+			if (currentTurn == 0) {
+				playerList1[currentPointer[currentTurn]] = tableName[currentPointer[currentTurn]];
+				for(int j=0;j<playerList1.length;j++) {
+					System.out.print(playerList1[j] + "\t");
 				}
-				else
-				{
-					playerlist2[l]=tableName[currentPointer[currentTurn]];
-					l++;	
-				}	
+			} else {
+				playerList2[currentPointer[currentTurn]] = tableName[currentPointer[currentTurn]];
+				for(int j=0;j<playerList2.length;j++) {
+					System.out.print(playerList2[j] + "\t");
+				}
+			}
+
 			// Code need to come in bottom before for ends
 			currentTurn = (currentTurn == 0) ? 1 : 0;
 			/*for(int s=0;s<=playerlist1.length-1;s++)  //to print the array of a player
@@ -113,15 +124,18 @@ class Monopoly_1{
 				System.out.print(playerlist1[s]);
 				System.out.print("\t");
 			}*/		
+			
 			System.out.print("Enter 1 to continue & 0 to exit"); //to conitnuew the program or exit the program
+			
 			Scanner cont=new Scanner(System.in);
 			i=cont.nextInt();
-			if(i==0)
-			break;
+
+			if (i==0)
+				break;
 		}
 	}
 
-	public static String startgame() {
+	public static String getPlayerName() {
 
 		System.out.print("Enter the Player Name: ");
 
@@ -131,7 +145,7 @@ class Monopoly_1{
 		return p1name;
 	}	
 	
-	public static int p1dice()
+	public static int diceRoll()
 	{
 		double rand1 = Math.random()*6+1; //Random value for dice 1 & 2 bet 1 to 6
 		
